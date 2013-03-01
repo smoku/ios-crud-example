7ffd1cddda20058eaff5a20e1aff39bcfd802e06
Paweł Smoczyk <pawel@georun.com>
2013-03-01 14:09:07 +0100

    RestKit configuration

---
+++ crud/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  crud
+//
+//  Created by Pawel Smoczyk on 01.03.2013.
+//  Copyright (c) 2013 GeoRun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
