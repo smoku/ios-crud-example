83e9d376587916a1a8b7e60b3bab4be55781f256
Paweł Smoczyk <pawel@georun.com>
2013-03-02 11:32:09 +0100

    edit tableview

--- crud/UI/PostsViewController.m
+++ crud/UI/PostsViewController.m
@@ -18,6 +18,8 @@
     
     [self setTitle:@"Posts"];
     
+    [self.navigationItem setLeftBarButtonItem:[self editButtonItem]];
+    
     _refreshControl = [[ODRefreshControl alloc] initInScrollView:self.tableView];
     [_refreshControl addTarget:self action:@selector(reloadRemoteData) forControlEvents:UIControlEventValueChanged];
 
@@ -83,44 +85,20 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+        Post *post = [_fetchResultsController objectAtIndexPath:indexPath];
+        NSString *resourcePath = [NSString stringWithFormat:@"api/posts/%@", post.guid];
+        [[RKObjectManager sharedManager] deleteObject:post path:resourcePath parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
+            
+            
+        } failure:^(RKObjectRequestOperation *operation, NSError *error) {
+            
+        }];
+    }
 }
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - NSFetchResultsControllerDelegate
 
